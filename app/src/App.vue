0c1e92ab8214be685018d7da26c1d9452d0cec60
humd1 <humdb@nycstudents.net>
2026-02-04 11:44:27 -0500

    started

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Cookie Clicker</h1>
+    <img  @click = "increment" src="/cookie.png" alt="Cookie" />
+    <h2>{{count}}</h2>\
+    <button @click = "count = 0"> reset </button>
+  </div>
+</template>
+
+<script setup> /*javascript*/
+import {ref} from 'vue'
+const count = ref(0)
+function increment(){
+  count.value++
+}
+</script>
+
+<style scoped> /*css*/
+img {
+  width: 200px;
+}
+
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 40vh;
+}
+
+</style>
