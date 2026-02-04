592689d6423d1bce1925c577b505adfef6d9c300
humd1 <humdb@nycstudents.net>
2026-02-04 14:08:36 -0500

    path

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CookieClicker from '@/views/CookieClicker.vue'
+import UserCreate from '@/views/UserCreate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,13 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: CookieClicker,
+    },
+    {
+      path: '/usercreate',
+      name: 'usercreate',
+      component: UserCreate,
     }
+
   ],
 })
 
---
+++ app/src/views/UserCreate.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <form action = "submit" @submit.prevent = "login(user)">
+        <label for="username">Username</label>
+        <input type ="text" v-model="user.username"/>
+        <label for="password">Password</label>
+        <input type ="password" v-model="user.password"/>
+    </form>
+    <h2 v-if ="loggedin"> {{user.username}} logged in!</h2>
+    <h2 v-else>log in pls</h2> 
+    </div>
+</template>
+
+<script setup>
+import {reactive} from 'vue'
+const user = reactive({username: '', password: ''})
+
+let loggedin = ref(false)
+
+function login(user){
+    console.log(user)
+    loggedin.value = true
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
